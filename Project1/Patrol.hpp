067f14a85776d58374d3bc831c2f2357381645c4
ElMino94 <trestoux@gaming.tech>
2025-02-18 15:58:12 +0100

    aaa

--- Project1/Patrol.hpp
+++ Project1/Patrol.hpp
@@ -1,19 +1,19 @@
 #pragma once
 
 #include "Enemy.hpp"
+#include "Grid.hpp"
+#include "BehaviorTree.hpp"
 #include <vector>
+#include <memory>
 
 using namespace sf;
 using namespace std;
 
-enum class PatrolState {
-	PATROLLING,
-	CHASING
-};
+
 
 class Patrol : public Enemy {
 private:
-	/*PatrolState state = PatrolState::PATROLLING;*/
+	
 	vector<Vector2f> waypoints;
 	int currentWaypointIndex = 0;
 	float detectionRadius = 100.0f;
@@ -24,7 +24,5 @@ public:
 
 	void update(float deltaTime, Grid& grid, Vector2f playerPos);
 	void setWaypoints(const vector<Vector2f>& newWaypoints);
-	/*void chasePlayer(float deltaTime);
-	void patrol(float deltaTime);
-	bool canSeePlayer(Grid& grid);*/
+	
 };
